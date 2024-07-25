c8d1b654fafcf8661751033ff5f33facf5fc1bf3
Divyanshu Jindal. <divyanshjindal.05@gmail.com.>
2024-07-25 21:05:28 +0530

    added my notes

---
+++ arr.js
@@ -0,0 +1,64 @@
+// let hero =["a","b","c","d","e","f"];
+// for(let i=0; i<hero.length; i++){
+//     console.log(hero[i]);
+// }
+
+// //for of 
+
+// let phone=["apple","samsung","oppo","IQOO","vivo"];
+// for(let mobile of phone){
+//     console.log(mobile);
+// }
+
+// //average of array elements
+
+// let marks=[22,33,55,66,87,3,46];
+// let sum=0;
+// for(mark of marks){
+//     sum += mark;
+// }
+// console.log(sum);
+// let avg = sum/marks.length;
+// console.log(avg);
+
+// //appling offer of 10%
+
+// let price = [200,350,980,360,120];
+// for(let amount of price){
+//     console.log(amount-amount/10);
+// }
+
+//using push and unshift
+
+// let phoneNumber=[7,8,3,6,8,3,6,9,4,9];
+// phoneNumber.push(0);
+// console.log(phoneNumber);
+
+// phoneNumber.unshift(78);
+// console.log(phoneNumber);
+
+// //using pop and shift 
+// phoneNumber.pop();
+// console.log(phoneNumber);
+
+
+// phoneNumber.shift();
+// console.log(phoneNumber);
+
+// using slice and splice
+// phoneNumber.slice(5,9);
+// console.log(phoneNumber.slice(5,9));
+// console.log(phoneNumber.splice(5,4,1,2,3,4));
+
+////practice question
+
+// let company=["bloomberg","microsoft","uber","google","ibm","netlix"];
+// //Q1 remove the first company
+// company.shift();
+// console.log(company);
+// //Q2 rem uber and ola
+// company.splice(2,1,"ola");
+// console.log(company);
+// //Q3 add amazon at end
+// company.push("amazon");
+// console.log(company);
---
+++ var1.js
@@ -0,0 +1,67 @@
+// const student={
+//     fullName :"divyansh",
+//     age : 19,
+//     usingGit: true,
+// };
+// console.log(student.age);
+// console.log(student["fullName"]);
+// console.log(student);
+// console.log(typeof student);
+
+// // for in loop maiinly used for objects
+
+// for(let val in student){
+//     console.log("key is :",val ,"value of", val,"is :",student[val]);
+// }
+
+// // for of loop maiinly used for strings
+
+// let naME="Divyanshjindal";
+// for(let fr of naME){
+//     console.log(fr);
+//     let size=0;
+//     size++;
+// }
+// console.log(size);
+
+// // all even number btw 0 to 100
+
+// for(let i=1; i<=100; i++){
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+// }
+
+// make a guess game
+//prompt are always strings
+
+let numb =7;
+let usernum = prompt("enter your guess");
+
+
+while(usernum!=numb){
+    usernum = prompt("you enter the wrong number");
+}
+
+    console.log("woow!!");
+
+//template literls
+
+// let obj={
+//     tittes: "pen",
+//     size: 36
+// };
+// let output1=`the size of ${obj.tittes}is ${obj.size} inches`;
+// console.log(output1);
+
+// //sting functions
+
+// let str="devyansh";
+// console.log(str);
+// console.log(str.replace("e","i"));
+
+// //making a mail id
+
+// let input=prompt("enter your full name");
+// let output=`@${input.toLowerCase()}${input.length}`;
+// console.log(output);
