0ab9685e761b948f33a40d922f0e9d9ee6790f1f
Divyanshu Jindal. <divyanshjindal.05@gmail.com.>
2024-07-30 13:46:06 +0530

    started dom

--- DOM.js
+++ DOM.js
@@ -1,8 +1,21 @@
-// //added cum back
+// // //added cum back
 
-let docTitle = document.title;
-window.addEventListener("blur", () => {document.title = "anty munty sunty";
-})
-window.addEventListener("focus", () => {
-document.title = docTitle;
-})
+// let docTitle = document.title;
+// window.addEventListener("blur", () => {document.title = "anty munty sunty";
+// })
+// window.addEventListener("focus", () => {
+// document.title = docTitle;
+// })
+
+// let firstEle = document.querySelector(".ghar");
+// console.dir(firstEle);
+// console.log(firstEle);
+
+// let heading = document.querySelector("h1");
+// console.log(heading);
+// heading.innerText = "new name is devu";
+// heading.innerHTML = "<i>my name is devu</i>";
+
+let h2 = document.querySelector("h2");
+console.dir(h2);
+h2.innerText = h2.innerText+"from dj";
